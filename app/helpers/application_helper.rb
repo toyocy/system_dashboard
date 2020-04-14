c079cfec8b44bd998c876dea36f0265b4515a30a
Taka, Toyoshima <ttoyoshima@prestigein.com>
2020-04-14 19:26:46 +0900

    edit system views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "System Dashboard"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
   def admin_full_name(admin)
     "#{admin.username}: #{admin.last_name} #{admin.first_name}"
   end
